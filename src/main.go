0314f2f69dc1168474d9e1f3aeeec2d474c226af
DeveloperC <DeveloperC@protonmail.com>
2022-08-07 01:46:05 +0100

    feat: repositories searching

--- src/main.go
+++ src/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	seachingCli := flag.String("searching", "", "The path to search for Git reposoties within.")
+	flag.Parse()
+
+	seaching := *seachingCli
+
+	if *seachingCli == "" {
+		currentWorkingDirectory, err := os.Getwd()
+
+		if err != nil {
+			panic(err)
+		}
+
+		seaching = currentWorkingDirectory
+	}
+
+	repositories := getRepositories(seaching)
+	fmt.Println(repositories)
+}
+
+func getRepositories(seaching string) []string {
+	var repositories []string
+
+	entries, err := os.ReadDir(seaching)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			path := seaching + "/" + entry.Name()
+
+			if entry.Name() == ".git" {
+				repositories = append(repositories, path)
+			} else {
+				repositories = append(repositories, getRepositories(path)...)
+			}
+		}
+	}
+
+	return repositories
 }
